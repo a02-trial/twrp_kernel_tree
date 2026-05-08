1f9854a459aacec75c76bc836ff24fa7c85e3e18
rdbckp <ardibackup@gmail.com>
2026-05-08 13:38:49 +0700

    Update twrp_a02.mk
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- twrp_a02.mk
+++ twrp_a02.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/samsung/a02/device.mk)
 PRODUCT_DEVICE := a02
 PRODUCT_NAME := twrp_a02
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := SM-A022F
+PRODUCT_MODEL := SM-A022M
 PRODUCT_MANUFACTURER := samsung
 
 PRODUCT_GMS_CLIENTID_BASE := android-samsung-ss
