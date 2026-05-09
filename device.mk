7a3ec112decff8c9d25ba33755d77df41973f871
rdbckp <ardibackup@gmail.com>
2026-05-09 14:41:46 +0700

    Update device.mk
    Signed-off-by: rdbckp <ardibackup@gmail.com>

--- device.mk
+++ device.mk
@@ -4,15 +4,13 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-
 LOCAL_PATH := device/samsung/a02
-
 # --- Bagian Copy Files ---
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/root/system/etc/ueventd.rc:recovery/root/ueventd.rc \
     $(LOCAL_PATH)/root/init.recovery.mt6739.rc:recovery/root/init.recovery.mt6739.rc \
     $(LOCAL_PATH)/root/init.recovery.samsung.rc:recovery/root/init.recovery.samsung.rc \
     $(LOCAL_PATH)/root/init.recovery.usb.rc:recovery/root/init.recovery.usb.rc \
-    $(LOCAL_PATH)/root/prop.default:recovery/root/prop.default
+    $(LOCAL_PATH)/root/prop.default:recovery/root/prop.default \
     $(LOCAL_PATH)/root/system/etc/recovery.fstab:recovery/root/system/etc/recovery.fstab \
     $(LOCAL_PATH)/root/system/etc/twrp.flags:recovery/root/system/etc/twrp.flags
